fd5a626d8bd7ad1839acbc192e0722e70c4e76c2
Timo Erdelt <erdelt@cip.ifi.lmu.de>
2018-02-14 16:23:24 +0100

    rest routes

---
+++ frontend/src/main/scala/weatherApp/models/MusicModels.scala
@@ -0,0 +1,62 @@
+package weatherApp.models
+
+import java.time.LocalDateTime
+import java.util.UUID
+
+
+case class PartyCreateResponse
+(
+  id: String,
+  name: String,
+  password: String,
+  createdAt: LocalDateTime
+)
+
+case class Song
+(
+  id: Int,
+  //id: Long,
+  streamingServiceID: String,
+  name: String,
+  artist: String,
+  album: String,
+  albumCoverUrl: String,
+  upvotes: Int,
+  downvotes: Int,
+  played: Boolean
+  //, createdAt: LocalDateTime
+)
+
+case class SongResponse(
+                         song: List[Song]
+                       )
+
+case class SongListElement
+(
+  name: String,
+  artist: String
+)
+
+case class SendSong
+(
+  streamingServiceID: String,
+  name: String,
+  artist: String,
+  album: String,
+  albumCoverUrl: String
+)
+
+case class SendSongVote
+(
+  partyID: String,
+  songID: Long,
+  positive: Boolean,
+  voteType: String
+)
+
+case class SetSongPlayed
+(
+  id: Long,
+  partyID: String
+)
+
