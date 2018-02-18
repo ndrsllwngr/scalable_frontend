7a8049e7b4e6d22124e7bda9389d01228d87b8ef
Andreas Ellwanger <andreas.ellwanger@campus.lmu.de>
2018-02-18 18:13:32 +0100

    Added logo

--- frontend/src/main/scala/scalable/pages/StartPage.scala
+++ frontend/src/main/scala/scalable/pages/StartPage.scala
@@ -49,20 +49,23 @@ object StartPage {
       <.div(
         ^.cls := "d-flex flex-column align-items-center justify-content-center",
         ^.height := "100vh",
-        <.div( // LOGO
-          ^.cls := "peeledLogo d-flex flex-row align-items-center mb-5 mt-0",
+        <.img( // LOGO
+          ^.cls := "img-fluid mb-5 mt-0",
+          ^.marginBottom := "35%",
           ^.flex := "0 0 auto",
-          <.p(
-            ^.aria.label := "CodePen"
-            ,<.span(dataTxtAttr := "S","S")
-            ,<.span(dataTxtAttr := "C","C")
-            ,<.span(dataTxtAttr := "A","A")
-            ,<.span(dataTxtAttr := "L","L")
-            ,<.span(dataTxtAttr := "A","A")
-            ,<.span(dataTxtAttr := "B","B")
-            ,<.span(dataTxtAttr := "L","L")
-            ,<.span(dataTxtAttr := "E","E")
-          )
+          ^.maxWidth := 150.px,
+          ^.src := "images/scalable.svg"
+//          <.p(
+//            ^.aria.label := "CodePen"
+//            ,<.span(dataTxtAttr := "S","S")
+//            ,<.span(dataTxtAttr := "C","C")
+//            ,<.span(dataTxtAttr := "A","A")
+//            ,<.span(dataTxtAttr := "L","L")
+//            ,<.span(dataTxtAttr := "A","A")
+//            ,<.span(dataTxtAttr := "B","B")
+//            ,<.span(dataTxtAttr := "L","L")
+//            ,<.span(dataTxtAttr := "E","E")
+//          )
         ),
           <.button(^.`type` := "button", ^.cls := "btn btn-success btn-block",
             ^.onClick --> navigateToJoinPage() ,
